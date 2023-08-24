e153b046a22e122bd9c8035e57d5f68b1b09edc1
Millz98 <dave_m86@hotmail.com>
2023-08-24 00:59:07 -0600

    more app js changes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { PrettyChatWindow } from "react-chat-engine-pretty";
 
 const ChatsPage = () => {
   return (
+    <div style={{ height: "100vh" }}>
     <PrettyChatWindow
     projectId="ccb4ba82-4356-4209-b5c8-c3d876aea758"
     username="zack1234"
     secret="pass1234"
-    style={{ height: "100vh" }}
+    style={{ height: "100%" }}
     />
+    </div>
    );
   };
 
