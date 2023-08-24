fe4a927d4c30fe1c4c889715807ce346fb742340
Millz98 <dave_m86@hotmail.com>
2023-08-24 10:25:03 -0600

    more updates and files

--- src/chatsPage.js
+++ src/chatsPage.js
@@ -4,7 +4,7 @@ const ChatsPage = (props) => {
   return (
     <div style={{ height: "100vh", width: "100vw" }}>
       <PrettyChatWindow
-        projectId={process.env.REACT_APP_CHAT_ENGINE_PROJECT_ID}
+        projectId="ccb4ba82-4356-4209-b5c8-c3d876aea758"
         username={props.user.username} // adam
         secret={props.user.secret} // pass1234
         style={{ height: "100%" }}
